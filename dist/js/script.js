14b89f8308e606c5e9550ff41decd0112e01e9cd
agung_aji <agungajipradana@gmail.com>
2023-02-28 13:07:23 +0700

    portfolio commit

---
+++ dist/js/script.js
@@ -0,0 +1,57 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Hamburger;
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// tabs portfolio
+
+const tabButtons = document.querySelectorAll(".container .tabMenu button");
+const tabPanels = document.querySelectorAll(".container .tabPanel");
+
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "#6d28d9";
+  tabPanels.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
+showPanel(0);
+
+// Load More Button
+let loadMoreBtn = document.querySelector("#loadMore");
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".tabPanel .box-container .content")];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 3;
+
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
